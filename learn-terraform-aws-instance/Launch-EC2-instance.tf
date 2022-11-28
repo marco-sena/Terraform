d2c2250fdaa439e54973c54879c898e325550ed1
marco-sena <masca@cin.ufpe.br>
2022-11-28 11:39:54 -0300

    Input variables
    Update the code so that the instance name is a input variable

--- learn-terraform-aws-instance/Launch-EC2-instance.tf
+++ learn-terraform-aws-instance/Launch-EC2-instance.tf
@@ -14,10 +14,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform-Instance"
+    Name = var.instance_name
   }
 }
